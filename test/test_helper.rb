dd87f135069b5019da3ffdf6e1a2f0e78e26a66d
Maciek Paliński <maciekpalinski@gmail.com>
2014-11-20 21:04:15 +0000

    add colours

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/autorun'
+require 'minitest/pride'
 require 'mocha/test_unit'
 
 class ActiveSupport::TestCase
